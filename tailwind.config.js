5e608c6ac3daac43af12248a6c1c24adfb127113
Reysil Clarion <rmclarion@PHILKOEI.COM.PH>
2024-03-20 15:02:31 +0800

    Second Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
@@ -13,9 +12,35 @@ module.exports = {
         '.scrollbar-hidden::-webkit-scrollbar': {
           display: 'none',
         },
+        '.outline-none': {
+          outline: 'none',
+        },
+        '.modal::backdrop': {
+          background: 'linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(54, 54, 54, 0.5))',
+          'backdrop-filter': 'blur(3px)',
+        },
+        '.box-shadow': {
+          boxShadow: '0 5px 10px 0 rgba(35, 35, 35, 0.5), 5px 0 10px 0 rgba(35, 35, 35, 0.5)',
+        },
+        '@keyframes appear': {
+          '0%': {
+            opacity: 0,
+            transform: 'scale(0.5)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'scale(1)',
+          },
+        },
+        '.animate-appear': {
+          animation: 'appear 0.3s ease-in-out forwards',
+        },
+        '.text-select-none': {
+          'user-select': 'none',
+        },
       };
       
-      addUtilities(newUtilities, ['responsive', 'hover']);
+      addUtilities(newUtilities, ['responsive', 'hover', 'motion-safe']);
     },
   ],
 };
